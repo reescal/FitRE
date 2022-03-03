228cffc0a93bacae06a8228194afcfbeb3ac5b3a
Roberto Escalera <roberto.escalera@headspring.com>
2022-03-03 13:20:43 -0600

    Initial commit of unit test project using Shouldly and Fixie.

---
+++ src/FitRE.Tests/InitialTests.cs
@@ -0,0 +1,13 @@
+﻿namespace FitRE.Tests
+{
+    using Shouldly;
+
+    public class InitialTests
+    {
+        public void ShouldPass()
+        {
+            bool b = true;
+            b.ShouldBeTrue();
+        }
+    }
+}
