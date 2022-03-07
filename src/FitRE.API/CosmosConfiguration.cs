ef4b33ed922a7a8781df6d37fd5c86efe4a1f9c0
Roberto Escalera <roberto.escalera@headspring.com>
2022-03-07 10:43:16 -0600

    Created startup for azure function to do DI of configuration and CosmosDB.

---
+++ src/FitRE.API/CosmosConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace FitRE.API;
+
+public class CosmosConfiguration
+{
+    public string Database { get; set; }
+    public string Container { get; set; }
+}
---
+++ src/FitRE.API/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using FitRE.API;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace FitRE.API;
+
+public class Startup : FunctionsStartup
+{
+    private static readonly IConfigurationRoot Configuration = new ConfigurationBuilder()
+        .SetBasePath(Environment.CurrentDirectory)
+        .AddJsonFile("appsettings.json", true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .Build();
+
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddSingleton(s => {
+            var connectionString = Configuration["CosmosDBConnection"];
+            if (string.IsNullOrEmpty(connectionString))
+                throw new InvalidOperationException(
+                    "Please specify a valid CosmosDBConnection in the appSettings.json file or your Azure Functions Settings.");
+
+            return new CosmosClientBuilder(connectionString)
+                .Build();
+        });
+
+        builder.Services.AddOptions<CosmosConfiguration>()
+            .Configure<IConfiguration>((settings, configuration) =>
+            {
+                configuration.GetSection("CosmosConfiguration").Bind(settings);
+            });
+    }
+}
